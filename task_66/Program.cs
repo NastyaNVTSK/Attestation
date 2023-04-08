ba66852a769bffd178f8677728f5e4415fa8b60e
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-04-09 11:32:39 +1200

    task_66

---
+++ task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿int Summ(int N, int M)
+{
+if (N==M)
+return N;
+else return N + Summ(N + 1, M);
+}
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех целых чисел от {N} до {M} равна {Summ(N,M)}");
