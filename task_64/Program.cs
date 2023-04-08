e1f9ceac7b1dadfb0ef72eca1dd0dabd5c92ff7d
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-04-09 11:09:53 +1200

    task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,11 @@
+﻿string ValueRec(int N)
+    {
+        if (N == 0) return " ";
+        return Convert.ToString (N) + " " + ValueRec(N-1);
+    }
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ValueRec(N));
+
+    
