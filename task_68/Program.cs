9f5a293f67c875dafe504be1703ca2c91a598275
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-04-09 11:43:05 +1200

    task_68

---
+++ task_68/Program.cs
@@ -0,0 +1,12 @@
+﻿int AkkRec(int N, int M)
+{
+if (N == 0) return M + 1;
+else if (M == 0) return AkkRec(N - 1, 1);
+else return AkkRec(N - 1, AkkRec(N, M - 1));
+}
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Значение функции Аккермана равно: {AkkRec(N,M)}");
